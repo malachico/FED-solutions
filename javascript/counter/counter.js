434ab81d3e3b9ba3cd0c2d2b990ca0f425856fda
malachic <malachic@wix.com>
2018-06-06 18:23:29 +0300

    refactored

--- javascript/counter/counter.js
+++ javascript/counter/counter.js
@@ -1,42 +1,39 @@
-window.onload = function() {
+window.onload = function () {
 
-    // Create a handler according to action
-    function createHandler(action, element){
+        // Create a handler according to action
+    function createHandler(action, element) {
         return () => {
-            let current = element.textContent;
+            let current = Number(element.textContent);
 
-            current = Number(current);
-            console.log(current);
-            if (action === 'increase') {
-                element.innerHTML = current + 1;
-            }
-            else if (action === 'decrease') {
+            action === 'increase' ?
+                element.innerHTML = current + 1 :
                 element.innerHTML = current - 1;
-            }
-        };
+        }
     }
 
-    // Create button with event handler
-    function createButton (name, eventHandler) {
+        // Create button with event handler
+    function createButton(name, eventHandler, parent) {
         let button = document.createElement("button");
         button.innerHTML = name;
 
-        let body = document.getElementsByTagName("body")[0];
-        body.appendChild(button);
+        parent.appendChild(button);
 
-        button.addEventListener ("click", eventHandler);
+        button.addEventListener("click", eventHandler);
     }
 
     // Create paragraph to display counter
     let counterText = document.createElement("p");
 
+    let body = document.getElementsByTagName("body")[0];
+
     // Set initial counter
     counterText.innerHTML = '0';
 
     // Create buttons attached to handlers
-    createButton("increase", createHandler('increase', counterText));
-    createButton("decrease", createHandler('decrease', counterText));
+    createButton("increase", createHandler('increase', counterText), body);
+    createButton("decrease", createHandler('decrease', counterText), body);
 
     // Append to body
-    document.getElementsByTagName("body")[0].appendChild(counterText);
-};
+    body.appendChild(counterText);
+}
+;
