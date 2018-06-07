e83da25c342c4cd53d629c6d38385d56eea938e3
malachic <malachic@wix.com>
2018-06-07 14:16:56 +0300

    added eslint empty file

--- testing/FizzBuzz.js
+++ testing/FizzBuzz.js
@@ -1,20 +1,18 @@
-let fizzbuzz = require("./FizzBuzz");
-
-exports.fb = function (i) {
-    switch (true) {
+exports.fb = function( i ) {
+    switch ( true ) {
         case i % 3 === 0 && i % 5 === 0:
-            return 'FizzBuzz';
+            return "FizzBuzz";
         case i % 3 === 0:
-            return 'Fizz';
+            return "Fizz";
         case i % 5 === 0:
-            return 'Buzz';
+            return "Buzz";
     }
 };
 
 
 // 1. Implement FizzBuzz function.
 function FizzBuzz() {
-    for (let i = 1; i <= 100; i++) {
-        console.log(i + " : " + fb(i));
+    for ( let i = 1; i <= 100; i++ ) {
+        console.log( `${i } : ${ fb( i )}` );
     }
 }
