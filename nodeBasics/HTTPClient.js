37e628053981ad7b5d2b5fa04f04f9a1de72e9fd
malachic <malachic@wix.com>
2018-06-10 15:52:02 +0300

    7 done

---
+++ nodeBasics/HTTPClient.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const url = process.argv[2];
+
+
+http.get(url, function (response) {
+    response.setEncoding("utf8");
+    response.on("data", (data) =>{
+        console.log(data);
+    })
+});
