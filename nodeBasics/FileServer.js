02eecd8c225cde1997c0f9016e5d778bac50b140
malachic <malachic@wix.com>
2018-06-11 09:48:17 +0300

    11 done

---
+++ nodeBasics/FileServer.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.argv[2];
+const filePath = process.argv[3];
+
+const fileStream = fs.createReadStream(filePath);
+
+http.createServer(function callback (request, response) {
+    fileStream.pipe(response);
+}).listen(port);
+
+
+
