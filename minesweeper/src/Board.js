186ea98dd926cf11b21f9d1db3043817062dedb0
malachic <malachic@wix.com>
2018-06-26 10:10:38 +0300

    moved createTable up to board

--- minesweeper/src/Board.js
+++ minesweeper/src/Board.js
@@ -2,12 +2,27 @@ import React from 'react';
 import BoardHeader from "./BoardHeader";
 import './css/board-header.css';
 import SquaresBoard from "./SquaresBoard";
+import Square from "./Square";
 
 
 export default class Board extends React.Component {
     constructor(props) {
         super(props);
+
     }
+
+    createTable = () => {
+        let table = [];
+
+        for (let i = 0; i < this.props.height; i++) {
+            let children = [];
+            for (let j = 0; j < this.props.width; j++) {
+                children.push(<Square width={i} height={j} value={null} handleClick={this.handleClick}/>)
+            }
+            table.push(<tr>{children}</tr>)
+        }
+        return table
+    };
 
     handleClick(i,j){
         console.log(i,j);
@@ -17,7 +32,7 @@ export default class Board extends React.Component {
         return (
             <div className="board">
                 <BoardHeader className="board-header" mines={this.props.mines}/>
-                <SquaresBoard className="squares-board" width={this.props.width} height={this.props.height} handleClick={this.handleClick}/>
+                <SquaresBoard className="squares-board" width={this.props.width} height={this.props.height} createTable={this.createTable} handleClick={this.handleClick}/>
             </div>
         );
     }
--- minesweeper/src/SquaresBoard.js
+++ minesweeper/src/SquaresBoard.js
@@ -11,25 +11,14 @@ export default class SquaresBoard extends React.Component {
     }
 
 
-    createTable = () => {
-        let table = [];
 
-        for (let i = 0; i < this.props.height; i++) {
-            let children = [];
-            for (let j = 0; j < this.props.width; j++) {
-                children.push(<Square width={i} height={j} value={null} handleClick={this.props.handleClick}/>)
-            }
-            table.push(<tr>{children}</tr>)
-        }
-        return table
-    };
 
 
     render() {
         return (
             <table className="squares-board">
                 <tr>
-                    {this.createTable()}
+                    {this.props.createTable()}
                 </tr>
             </table>
         );
