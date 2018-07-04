d1db7d7cd6c1a19678810b7cb3f223695a1ff85a
malachic <malachic@wix.com>
2018-07-04 13:48:38 +0300

    refactor header components to be pure

--- minesweeper/src/BombsCounter.js
+++ minesweeper/src/BombsCounter.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import "./css/header.css"
-import bomb from "./images/bomb.png"
 
 export default class BombsCounter extends React.Component {
     render() {
-        return (
-            <span className="counter">
-                <span>{this.props.mines - this.props.flags}</span>
-                <img className="header-icon" src={bomb} alt={""}/>
-            </span>
-
-        );
     }
 }
--- minesweeper/src/Header.js
+++ minesweeper/src/Header.js
@@ -1,42 +1,65 @@
 import React from 'react';
-import BombsCounter from "./BombsCounter";
-import TimeCounter from "./TimeCounter";
-import MovesCounter from "./MovesCounter";
-import Smiley from "./Smiley";
 import "./css/header.css"
 import * as utils from "./utils";
+import plays from './images/plays.png'
+import timer from './images/timer.png'
+import bomb from "./images/bomb.png"
+import smiley from "./images/smiley.png";
 
 
 export default class Header extends React.Component {
 
     render() {
+        let counter;
 
         if (this.props.currentDisplay === utils.MOVES_COUNTER) {
-            return this.renderMoves();
+            counter = this.movesCounter();
         }
         if (this.props.currentDisplay === utils.TIME_COUNTER) {
-            return this.renderTime();
+            counter = this.timeCounter();
         }
-    }
 
-
-    renderTime() {
         return (
             <div className="counters">
-                <BombsCounter mines={this.props.mines} flags={this.props.flags}/>
-                <Smiley initNewGame={this.props.initNewGame}/>
-                <TimeCounter className="counter" timePassed={this.props.timePassed} switchDisplay={this.props.switchDisplay}/>
+                {this.bombsCounter()}
+                {this.smiley()}
+                {counter}
             </div>
         );
+
     }
 
-    renderMoves() {
+    bombsCounter = () => {
         return (
-            <div className="counters">
-                <BombsCounter mines={this.props.mines} flags={this.props.flags}/>
-                <Smiley initNewGame={this.props.initNewGame}/>
-                <MovesCounter className="counter" switchDisplay={this.props.switchDisplay} moves={this.props.moves}/>
-            </div>
+            <span className="counter">
+                <span>{this.props.mines - this.props.flags}</span>
+                <img className="header-icon" src={bomb} alt={""}/>
+            </span>
+
+        );
+    };
+
+    movesCounter = () => {
+        return (<button className='counter' onClick={() => this.props.switchDisplay()}>
+                    <span>
+                        <img className='header-icon' src={plays} alt=""/>
+                        <span>{this.props.moves}</span>
+                    </span>
+        </button>);
+    };
+
+    timeCounter = () => {
+        return <button className='counter' onClick={() => this.props.switchDisplay()}>
+            <span>
+                <img className='header-icon' src={timer} alt=""/>
+                <span>{this.props.timePassed}</span>
+            </span>
+        </button>
+    };
+
+    smiley = () => {
+        return (
+            <button className="smiley-button" onClick={this.props.initNewGame}><img src={smiley} alt={""}/></button>
         );
     }
 }
